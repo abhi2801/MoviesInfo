fcc5fcc2b9a0e1d57830ffb35b48cf6978f81f01
abhi2801 <abhishek.nayak560@gmail.com>
2023-08-22 12:17:32 +0530

    commiting from testing

---
+++ app/src/main/java/com/example/movies/utils/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.movies.utils
+
+object Constants {
+    const val baseurl=""
+    const val apikey=""
+    const val popularEndpoint=""
+    const val nowPlayingEndpint=""
+    const val upcomingEndpoint=""
+}
