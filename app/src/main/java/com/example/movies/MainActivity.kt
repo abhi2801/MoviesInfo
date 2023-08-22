13a3ffea55a4646752c99e63d931b5ab38b00cef
abhi2801 <abhishek.nayak560@gmail.com>
2023-08-22 12:27:31 +0530

    toast added in testing 1

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -11,5 +11,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Toast.makeText(this, "Hello", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "hi", Toast.LENGTH_SHORT).show()
     }
 }
